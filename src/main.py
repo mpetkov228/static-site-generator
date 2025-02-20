da88c2ee204ab98ee9aabb2ba7d0f93b2d288627
mpetkov228 <mpetkov228@gmail.com>
2025-02-20 15:31:56 +0200

    Add split_nodes_delimiter function

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from textnode import TextType, TextNode
 
 
 def main():
-    my_node = TextNode("text", TextType.NORMAL, "www.website.com")
+    my_node = TextNode("text", TextType.TEXT, "www.website.com")
     print(my_node)
 
 
---
+++ src/split_nodes.py
@@ -0,0 +1,29 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        parts = node.text.split(delimiter)
+        if len(parts) != 3:
+            raise Exception("missing closing delimiter")
+        
+        for i in range(len(parts)):
+            if i == 1:
+                new_nodes.append(TextNode(parts[1], text_type))
+            else:
+                new_nodes.append(TextNode(parts[i], TextType.TEXT))
+
+    return new_nodes
+
+
+node = TextNode("This is text with a `code block` word", TextType.TEXT)
+node2 = TextNode("This is text with a **bold word**", TextType.TEXT)
+node3 = TextNode("*Italic* text", TextType.TEXT)
+node4 = TextNode("Italic text 2", TextType.ITALIC)
+print(split_nodes_delimiter([node], "`", TextType.CODE))
+print(split_nodes_delimiter([node2], "**", TextType.BOLD))
+print(split_nodes_delimiter([node3, node4], "*", TextType.ITALIC))
