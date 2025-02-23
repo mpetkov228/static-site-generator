124c5da14b03777e95476cd7deb25de6ef0ef0e8
mpetkov228 <mpetkov228@gmail.com>
2025-02-23 18:38:13 +0200

    Restructure html nodes into one file

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,3 +21,36 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        self.tag = tag
+        self.children = children
+        self.props = props
+
+    
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("missing tag field")
+        if not self.children:
+            raise ValueError("missing children field")
+        
+        children = ""
+        for child in self.children:
+            children += child.to_html()
+        
+        return f'<{self.tag}{self.props_to_html()}>{children}</{self.tag}>'
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value=None, props=None):
+        super().__init__(tag, value, props=props)
+
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("missing object value")
+        if not self.tag:
+            return str(self.value)
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
