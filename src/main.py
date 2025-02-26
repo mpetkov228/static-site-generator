c0e0982e45cea10a6cf715254311ec0842da0e9d
mpetkov228 <mpetkov228@gmail.com>
2025-02-26 15:53:41 +0200

    Add public directory generator function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,39 @@
+import os
+import shutil
+
 from textnode import TextType, TextNode
+
+
+def copy_files(src, src_contents, dst):
+    for contents in src_contents:
+        path = os.path.join(src, contents)
+        if os.path.isfile(path):
+            shutil.copy(path, dst)
+            continue
+        new_dst = os.path.join(dst, contents)
+        os.mkdir(new_dst)
+
+        new_src = os.path.join(path)
+        new_contents = os.listdir(path)
+        copy_files(new_src, new_contents, new_dst)
+
+
+def generate_public():
+    public_path = "./public"
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+
+    os.mkdir("public")
+
+    static_path = "./static"
+    static_contents = os.listdir("./static")
+    copy_files(static_path, static_contents, public_path)
 
 
 def main():
     my_node = TextNode("text", TextType.TEXT, "www.website.com")
     print(my_node)
+    generate_public()
 
 
 main()
