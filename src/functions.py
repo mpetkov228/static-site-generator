178116b5dd296077da6cd19f1ca26ba4ea1d56e3
mpetkov228 <mpetkov228@gmail.com>
2025-02-26 16:39:27 +0200

    Add extract_title function

--- src/functions.py
+++ src/functions.py
@@ -9,3 +9,10 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!\!)\[(.*?)\]\((.*?)\)", text)
     return matches
+
+
+def extract_title(markdown):
+    match = re.match(r"^# .+", markdown)
+    if match:
+        return match.group().lstrip("# ")
+    raise Exception("no title found")
