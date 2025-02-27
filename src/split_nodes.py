62abb44cbf257917168bc5a0859af2a45c9a59d6
mpetkov228 <mpetkov228@gmail.com>
2025-02-27 16:34:25 +0200

    Refactor split_nodes_delimiter to handle multiple same inline nodes

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -2,28 +2,49 @@ from textnode import TextNode, TextType, text_node_to_html_node
 from functions import extract_markdown_images, extract_markdown_links
 
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    if delimiter != "```" and delimiter != "*" and delimiter != "**" and delimiter != "_":
-        raise Exception("unknown delimiter")
+# def split_nodes_delimiter(old_nodes, delimiter, text_type):
+#     if delimiter != "```" and delimiter != "*" and delimiter != "**" and delimiter != "_" and delimiter != "`":
+#         raise Exception("unknown delimiter")
     
-    new_nodes = []
-    for node in old_nodes:
-        if node.text == "":
-            continue
-        if delimiter not in node.text:
-            new_nodes.append(node)
-            continue
+#     new_nodes = []
+#     for node in old_nodes:
+#         if node.text == "":
+#             continue
+#         if delimiter not in node.text:
+#             new_nodes.append(node)
+#             continue
 
-        parts = node.text.split(delimiter)
-        if len(parts) < 2:
-            raise Exception("missing closing delimiter")
+#         parts = node.text.split(delimiter)
+#         print(parts)
+#         if len(parts) < 2:
+#             raise Exception("missing closing delimiter")
         
-        for i in range(len(parts)):
-            if i == 1:
-                new_nodes.append(TextNode(parts[1], text_type))
-            else:
-                new_nodes.append(TextNode(parts[i], TextType.TEXT))
+#         for i in range(len(parts)):
+#             if i == 1:
+#                 new_nodes.append(TextNode(parts[1], text_type))
+#             else:
+#                 new_nodes.append(TextNode(parts[i], TextType.TEXT))
+#     print(new_nodes)
+#     return new_nodes
 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
     return new_nodes
 
 
@@ -83,6 +104,7 @@ def text_to_textnodes(text):
     nodes_italic = split_nodes_delimiter(nodes_bold, "*", TextType.ITALIC)
     nodes_italic = split_nodes_delimiter(nodes_italic, "_", TextType.ITALIC)
     nodes_code = split_nodes_delimiter(nodes_italic, "```", TextType.CODE)
+    nodes_code = split_nodes_delimiter(nodes_code, "`", TextType.CODE)
     nodes_image = split_nodes_image(nodes_code)
     nodes_link = split_nodes_link(nodes_image)
     return nodes_link
